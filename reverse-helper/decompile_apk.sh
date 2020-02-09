912a07f0895391d69fe1601632f99c26e370770f
Elizaveta Ryzhenkova <elizavetaryzhenkova@Elizavetas-MacBook-Pro.local>
2020-02-09 14:18:18 +0300

    decompile script and dockerfile for helper finished

--- reverse-helper/decompile_apk.sh
+++ reverse-helper/decompile_apk.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-    
-  declare apk_location=${1}
-  if [[ $apk_file_location =~ (.+)(.apk) ]]; then
-    declare zip_file_location = ${BASH_REMATCH[1]}
+  apk_name=${1}
+  regex='(.+).apk'
+  
+  if [[ $apk_name =~ $regex ]]; then
+    zip_ext='.zip'
+    zip_name=${BASH_REMATCH[1]}$zip_ext
+    mv $apk_name $zip_name
+    unzip $zip_name -d unzipped
   else echo "could not parse file name"
   fi
 
-  unzip $zip_file_location -d unzipped
-  mkdir dex-tools
-  
-  curl "https://api.github.com/repos/pxb1988/dex2jar/releases/latest" \
+   mkdir dex-tools
+   curl "https://api.github.com/repos/pxb1988/dex2jar/releases/latest" \
     | grep "dex-tools-*.*.zip" \
     | cut -d : -f 2,3 \
     | tr -d \" \
-    | wget -qi - -O dex-tools/dex-tools.zip
-    
-  unzip dex-tools/dex-tools.zip -d dex-tools
-  rm dex-tools/dex-tools.zip
-  chmod -R +x dex-tools
-    dex_files
-    jar_files
-    # classes.dex in unzipped folder
-    $dex_files = $(ls unzipped -t -U | grep "dex")
-    for f in $dex_files
-      do
-        $jar_files += $(dex-tools/d2j-dex2jar $f)
-      done 
-    
-    # https://github.com/betterphp/JDCommandLine.git
-    for jf in $jar_files
-      do
-        java -jar JDCommandLine.jar $jf "${jf}_sources"
-      done
+    | wget -qi - -O dex-tools/dex-tools.zip 
 
-      # decompile your class files (e.g. with JD-GUI or DJ Decompilero)
+   unzip dex-tools/dex-tools.zip -d dex-tools
+   rm dex-tools/dex-tools.zip
+   chmod -R +x dex-tools
+
+   dex_files=$(ls unzipped | grep "dex")
+   cd unzipped
+   for d in $dex_files
+   do
+     ../dex-tools/dex2jar-2.0/d2j-dex2jar.sh $d
+   done
+
+   git clone https://github.com/betterphp/JDCommandLine.git
+
+   jar_files=$(ls unzipped | grep "jar")
+   for j in $jar_files
+   do
+     java -jar ~/JDCommandLine/JDCommandLine.jar $jf
+   done
+   
 else 
   echo APK location not specified
 fi
