1806938ebaf74f9937bfd720297983dabc515165
Elizaveta Ryzhenkova <elizavetaryzhenkova@Elizavetas-MacBook-Pro.local>
2020-02-09 11:26:18 +0300

    docker compose for static

--- reverse-helper/decompile_apk.sh
+++ reverse-helper/decompile_apk.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-    declare zip_file_location = "${1}.zip"
-    unzip $zip_file_location -d unzipped
-    mkdir dex-tools
-    curl "https://api.github.com/repos/pxb1988/dex2jar/releases/latest" \
-      | grep "dex-tools-*.*.zip" \
-      | cut -d : -f 2,3 \
-      | tr -d \" \
-      | wget -qi - -O dex-tools/dex-tools.zip
-    unzip dex-tools/dex-tools.zip -d dex-tools
-    rm dex-tools/dex-tools.zip
-    chmod -R +x dex-tools
+    
+  declare apk_location=${1}
+  if [[ $apk_file_location =~ (.+)(.apk) ]]; then
+    declare zip_file_location = ${BASH_REMATCH[1]}
+  else echo "could not parse file name"
+  fi
 
+  unzip $zip_file_location -d unzipped
+  mkdir dex-tools
+  
+  curl "https://api.github.com/repos/pxb1988/dex2jar/releases/latest" \
+    | grep "dex-tools-*.*.zip" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi - -O dex-tools/dex-tools.zip
+    
+  unzip dex-tools/dex-tools.zip -d dex-tools
+  rm dex-tools/dex-tools.zip
+  chmod -R +x dex-tools
+    dex_files
+    jar_files
     # classes.dex in unzipped folder
-    dex_files = ($(ls unzipped -t -U | grep "dex"))
-    jar_files = ()
+    $dex_files = $(ls unzipped -t -U | grep "dex")
     for f in $dex_files
       do
-        $jar_files += ($(dex-tools/d2j-dex2jar $f))
+        $jar_files += $(dex-tools/d2j-dex2jar $f)
       done 
     
     # https://github.com/betterphp/JDCommandLine.git
--- static-analysis-service/qark/README.md
+++
@@ -1,2 +0,0 @@
-## QARK docker container from dockerhub 
-https://hub.docker.com/r/ledokun/qark/
